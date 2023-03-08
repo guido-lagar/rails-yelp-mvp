9362c5f6597fbfd8f2b4342d4fcd0bdf843ae7ae
Guido Lagar <guidolagar@gmail.com>
2023-03-08 17:05:53 -0300

    not done yet

--- Gemfile
+++ Gemfile
@@ -18,6 +18,12 @@ gem "puma", "~> 5.0"
 # Use JavaScript with ESM import maps [https://github.com/rails/importmap-rails]
 gem "importmap-rails"
 
+# simple form
+gem "simple_form"
+
+#faker
+gem "faker"
+
 # Hotwire's SPA-like page accelerator [https://turbo.hotwired.dev]
 gem "turbo-rails"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'restaurants', to: 'restaurants#index', as: :restaurants
+  get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  post 'restaurants', to: 'restaurant#create'
+  get 'restaurant/:id', to: 'restaurants#show', as: :restaurant
 
+  # resources :reviews, only: %i[new create]
+  # end
   # Defines the root path route ("/")
   # root "articles#index"
 end
